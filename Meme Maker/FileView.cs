6aa81fbc618777f9664fa1d03c9e545c57a5a7f6
wilversings <aiordachioaei.marius@gmail.com>
2016-11-30 20:50:38 +0200

    Added multi file view

---
+++ Meme Maker/FileView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meme_Maker.Meme {
+    public partial class FileView : Form {
+        public FileView () {
+            InitializeComponent ();
+        }
+    }
+}
