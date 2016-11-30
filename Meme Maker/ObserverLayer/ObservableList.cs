5943fc33f9a9943d86e4fdeacb514118070fcb7d
wilversings <aiordachioaei.marius@gmail.com>
2016-11-30 22:34:39 +0200

    Further simplified the ObservableList class

---
+++ Meme Maker/ObserverLayer/ObservableList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemeMaker.ObserverLayer;
+using System.Collections.ObjectModel;
+
+namespace MemeMaker.ObserverLayer {
+    class ObservableList<T> : ObservableCollection<T> {
+        public ObservableList (Context notifyingContext, Subject obsSubject) : base() {
+            this.CollectionChanged += (sender, eventArgs) => {
+                obsSubject.NotifyAll (notifyingContext);
+            };
+        }
+    }
+}
